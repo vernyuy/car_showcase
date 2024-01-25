02234cd894bbd66e540ed007fa30595e9899e875
vernyuy <fonchu.e.venyuy@gmail.com>
2024-01-25 15:03:28 +0100

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { relative } from "path";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Navbar, Footer } from "@/componentsCised";
+import { Navbar, Footer } from "@/components";
 
 export const metadata: Metadata = {
   title: "Create Next App",

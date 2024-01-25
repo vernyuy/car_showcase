51f90d40c8a9a5c93caa159032612f0db317724c
vernyuy <fonchu.e.venyuy@gmail.com>
2024-01-25 15:21:12 +0100

    updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,7 +26,7 @@ module.exports = {
       },
       backgroundImage: {
         pattern: "url('/pattern.png')",
-        "hero-bg": "url('/cised-bg.png')",
+        "hero-bg": "url('/hero-bg.png')",
       },
     },
   },

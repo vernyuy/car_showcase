9f81f091646bf7c02cbdeeb8344a7a6948b8c390
vernyuy <fonchu.e.venyuy@gmail.com>
2024-01-26 02:41:17 +0100

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="relative overflow-x-hidden">
+      <body className="overflow-x-hidden">
         <Navbar />
         {children}
         <Footer />
